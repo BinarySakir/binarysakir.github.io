1907a74843d7f561bbd2b775b9dbcdc10a21de88
binarysakir <binarysakir@gmail.com>
2019-05-09 19:35:34 +0600

    exp panacea

---
+++ p2/js/main.js
@@ -0,0 +1,11 @@
+// ScrollReveal().reveal('.cnt-1 h5', {distance: '20px',});
+// ScrollReveal().reveal('.cnt-1 a', {
+// 	distance: '20px',
+// 	easing: 'ease',
+// 	delay: 200,
+// 	interval: 200 });
+// ScrollReveal().reveal('.cnt-2 .load-hidden', {
+// 	distance: '20px',
+// 	opactiy: 1,
+// 	easing: 'ease',
+// 	interval: 150 });
