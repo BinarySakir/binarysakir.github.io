8c285ba35efeeff56e98eb9c8fb1828e22c3ea84
binarysakir <binarysakir@gmail.com>
2019-05-13 01:38:38 +0600

    startw more inputs

--- p2-new/js/main.js
+++ p2-new/js/main.js
@@ -1,16 +1,30 @@
-// ScrollReveal().reveal('.cnt-1 h5', {distance: '20px',});
-// ScrollReveal().reveal('.cnt-1 a', {
-// 	distance: '20px',
-// 	easing: 'ease',
-// 	delay: 200,
-// 	interval: 200 });
-// ScrollReveal().reveal('.cnt-2 .load-hidden', {
-// 	distance: '20px',
-// 	opactiy: 1,
-// 	easing: 'ease',
-// 	interval: 150 });
 $(document).ready(function() {
 	window.sr = new ScrollReveal();
 	sr.reveal('.sr__b', { origin: 'bottom', interval: 150, distance: '20px' });
-	
+	var i = 1;
+	var j = 3;
+	$('.form_ div').hide();
+	$('.form_ .q1').show();
+	$('.form_ .q2, .form_ .q3').css({
+		'visibility': 'visible'
+	});
+	$('.btn__next').click(function() {
+	    if(i != j){
+		    $('.q'+(i).toString()).hide();
+		    $('.q'+(i+1).toString()).fadeIn("slow");
+		    i++;
+	    }else{
+	    	// $('.q'+(i).toString() + ',.btn__next').hide();
+	    	alert("Done!"); // Here redirect the user to the result page
+	    }
+	});
+	// $('.btn-back').click(function() {
+	//     if(i != 1){
+	// 	    $('.q'+(i).toString()).hide();
+	// 	    $('.q'+(i-1).toString()).fadeIn("slow");
+	// 	    i--;
+	//     }else{
+	//     	return false;
+	//     }
+	// });
 })
