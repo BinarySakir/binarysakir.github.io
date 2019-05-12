3db945a2127136ed9a147ba9361ce872b12a8025
binarysakir <binarysakir@gmail.com>
2019-05-13 00:26:57 +0600

    New theme

---
+++ p2-new/js/main.js
@@ -0,0 +1,19 @@
+// ScrollReveal().reveal('.cnt-1 h5', {distance: '20px',});
+// ScrollReveal().reveal('.cnt-1 a', {
+// 	distance: '20px',
+// 	easing: 'ease',
+// 	delay: 200,
+// 	interval: 200 });
+// ScrollReveal().reveal('.cnt-2 .load-hidden', {
+// 	distance: '20px',
+// 	opactiy: 1,
+// 	easing: 'ease',
+// 	interval: 150 });
+$(document).ready(function() {
+	window.sr = new ScrollReveal();
+	sr.reveal('.load-hidden', { origin: 'left', interval: 150, distance: '20px' });
+	// $('#form').submit(function(e) {
+ 	//    	show error or submit data
+	// });
+
+})
