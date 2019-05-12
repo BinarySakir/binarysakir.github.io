3e2d6dba952ef9281158d3965a265b0ea8d368eb
binarysakir <binarysakir@gmail.com>
2019-05-13 02:47:45 +0600

    startw more on form

--- p2-new/js/main.js
+++ p2-new/js/main.js
@@ -1,8 +1,17 @@
 $(document).ready(function() {
+	// Scroll Reveal
 	window.sr = new ScrollReveal();
 	sr.reveal('.sr__b', { origin: 'bottom', interval: 150, distance: '20px' });
+	
+	// Start warranty form
 	var i = 1;
 	var j = 3;
+	var errorMessages = {
+		1 : "Please enter a valid name.",
+		2 : "Please enter a valid phone number.",
+		3 : "Please enter a valid dealer code."
+	};
+	var e = 0;			// validation error
 	$('.form_ div').hide();
 	$('.form_ .q1').show();
 	$('.form_ .q2, .form_ .q3').css({
@@ -10,14 +19,20 @@ $(document).ready(function() {
 	});
 	$('.btn__next').click(function() {
 	    if(i != j){
-		    $('.q'+(i).toString()).hide();
-		    $('.q'+(i+1).toString()).fadeIn("slow");
-		    if(i === (j-1)){
-		    	$('.btn__next').text("Submit");
+		    if(e != 1){		// if there is no error
+			    $('.q'+(i).toString()).hide();
+			    $('.q'+(i+1).toString()).fadeIn("slow");
+			    if(i === (j-1)){
+			    	$('.btn__next').text("Submit");
+			    }
+			    i++;
+		    }else{		// if there is error
+		    	var p = '.q'+(i).toString();
+		    	$('.q'+(i).toString()).addClass('input__red');
+		    	$('.error').append(errorMessages[i]);
 		    }
-		    i++;
 	    }
-	    else{
+	    else{			// if it's the last input
 	    	alert("Done!"); // Here redirect the user to the result page
 	    }
 	});
