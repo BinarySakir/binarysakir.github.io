fd797975b15ad98f31c95a7240efe6f89a7aca1d
binarysakir <binarysakir@gmail.com>
2019-05-20 18:26:58 +0600

    verify

--- p2-new/js/main.js
+++ p2-new/js/main.js
@@ -4,31 +4,34 @@ $(document).ready(function() {
 	sr.reveal('.sr__b', { origin: 'bottom', interval: 150, distance: '20px' });
 	
 	// Start warranty form
+	// the validation process is just my idea
+	// let me know anything needs to be changed
 	var i = 1;
 	var j = 3;
 	var errorMessages = {
-		1 : "Please enter a valid name.",
+		1 : "Please enter a valid code.",
 		2 : "Please enter a valid phone number.",
 		3 : "Please enter a valid dealer code."
 	};
-	var e = 0;			// validation error
+	var e = 1;			// validation error
 	$('.form_ div').hide();
-	$('.form_ .q1').show();
-	$('.form_ .q2, .form_ .q3').css({
+	$('.form_ .qv1').show();
+	$('.form_ .qv2, .form_ .qv3').css({
 		'visibility': 'visible'
 	});
 	$('.btn__next').click(function() {
+		// call ajax and check if there's any error
+		// if there's an error, then set e = 1
 	    if(i != j){
 		    if(e != 1){		// if there is no error
-			    $('.q'+(i).toString()).hide();
-			    $('.q'+(i+1).toString()).fadeIn("slow");
+			    $('.qv'+(i).toString()).hide();
+			    $('.qv'+(i+1).toString()).fadeIn("slow");
 			    if(i === (j-1)){
 			    	$('.btn__next').text("Submit");
 			    }
 			    i++;
 		    }else{		// if there is error
-		    	var p = '.q'+(i).toString();
-		    	$('.q'+(i).toString()).addClass('input__red');
+		    	$('.qv'+(i).toString()).addClass('input__red');
 		    	$('.error').append(errorMessages[i]);
 		    }
 	    }
@@ -36,13 +39,13 @@ $(document).ready(function() {
 	    	alert("Done!"); // Here redirect the user to the result page
 	    }
 	});
-	// $('.btn-back').click(function() {
-	//     if(i != 1){
-	// 	    $('.q'+(i).toString()).hide();
-	// 	    $('.q'+(i-1).toString()).fadeIn("slow");
-	// 	    i--;
-	//     }else{
-	//     	return false;
-	//     }
-	// });
+	$('.btn-back').click(function() {
+	    if(i != 1){
+		    $('.qv'+(i).toString()).hide();
+		    $('.qv'+(i-1).toString()).fadeIn("slow");
+		    i--;
+	    }else{
+	    	return false;
+	    }
+	});
 })
