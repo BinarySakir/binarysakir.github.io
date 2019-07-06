0c91d03fcb37041cdf097c39c9d8364e39fcdac5
binarysakir <binarysakir@gmail.com>
2019-07-06 23:27:57 +0600

    Form Transition

---
+++ check/js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+	// Scroll Reveal
+	ScrollReveal().reveal('.sr__b', { origin: 'bottom', interval: 150, distance: '20px' });
+	
+	// Check warranty toggle form
+	var message_1 = "Perhaphs a warranty code?";
+	var message_2 = "Perhaphs a phone number?";
+	$('.switch-message').click(function(){
+		if($(".q1").is(":visible")){
+			$(".q1").hide();
+			$(".q2").css({"display": "initial"});
+			$(this).text(message_2);
+			// $(".switch-message").append(message_2);
+		}else{
+			$(".q2").hide();
+			$(".q1").css({"display": "initial"});
+			$(this).text(message_1);
+		}
+	});
+	
+});
