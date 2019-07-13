0cf4f1e4fca6ae5c16313a5c93c074d8e73cdb16
binarysakir <binarysakir@gmail.com>
2019-07-13 20:52:14 +0600

    Error Transition

--- check/js/main.js
+++ check/js/main.js
@@ -7,16 +7,25 @@ $(document).ready(function() {
 	var message_2 = "Perhaphs a phone number?";
 	$('.switch-message').click(function(){
 		if($(".q1").is(":visible")){
+			$(".error").hide();
 			$(".q1").hide();
-			$(".q2").css({"display": "initial"});
-			$(".input_").addClass("animated fadeInDown");
+			$(".q2").css({"display": "inherit"});
+			$(".q2").addClass("animated fadeInDown fast");
 			$(this).text(message_2);
 		}else{
+			$(".error").hide();
 			$(".q2").hide();
-			$(".q1").css({"display": "initial"});
-			$(".input_").addClass("animated fadeInDown");
+			$(".q1").css({"display": "inherit"});
+			$(".q1").addClass("animated fadeInDown fast");
 			$(this).text(message_1);
 		}
+	});
+	$('.btn__b').click(function(){
+		$(".error").css({"display": "inherit"});
+		$(".error").addClass("animated headShake");
+		setTimeout(function() { 
+	        $(".error").removeClass("animated headShake");
+	    }, 1000);
 	});
 	
 });
