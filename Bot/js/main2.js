1cbd0535f3478f7d08aa14c1128c3f92c973272b
binarysakir <binarysakir@gmail.com>
2020-04-06 21:17:03 +0600

    Initial Commit

--- Bot/js/main2.js
+++ Bot/js/main2.js
@@ -3,12 +3,13 @@ var block2 = "<div class=\"block_2\"><div class=\"conv-grp\"><img src=\"avatar.s
 $(".option").click(function(){
 	$(this).toggleClass("selected");
 });
-$(".block_2, .block_3, .block_4, .block_5, .block_6, .block_7").hide();
+$(".block_2, .block_3, .block_4, .block_5, .block_6, .block_7, .sp_1").hide();
 $(".block_1 .proceed").click(function(){
 	$(".selected").each( function(a){
 		$(".block_1 .right .conv").append("<div class='text'>" + $(".selected").eq(a).text() +"</div><br>");
 		$(".block_1 .drawer").addClass("slideoff").fadeOut(500);
 	});
+	$(".sp_1").show();
 	$(".block_2").show();
 });
 $(".block_2 .proceed").click(function(){
