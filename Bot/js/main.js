0e348b9e94750d1bd0e503931e1393eec7ceb10f
binarysakir <binarysakir@gmail.com>
2020-04-06 21:09:34 +0600

    Initial Commit

--- Bot/js/main.js
+++ Bot/js/main.js
@@ -1,13 +1,10 @@
 // sample.json in compact format
 $( document ).ready(function() {
-	var data = JSON.parse('{"quantity":4,"messages":[["Hi!","Welcome to our chat bot. Let\'s begin by asking some questions."],"","",""],"questions":["Are you experiencing any of these problems?","How old are you?","Are you experiencing any of these problems?","Do you have any of these conditions?","Have you travelled internationally?","In the last 14 days, have you been in an area where COVID-19 is widespread?","Tell us your exposure to covid-19 cases"],"options":[["Severe chest pain","Severe Disorientation","Constant chest pain","Difficulty breathing","Constant lightheadedness"],["Under 18","18 - 64","65 or above"],["Fever or sweating","Difficulty breathing","Sore throat","New & worsening cough","Vomiting/ diarrhea","None of the above"],["Asthma","Chronic lung disease","Diabetes","Pregnant?","Kidney failure","Obesity (Extreme)","Obesity (Moderate)","Liver Cirrhosis","Weakened immune system","Congestive Heart Failure","Heart Failure","Disease that makes you cough","Disease that makes you harder to cough"],["Yes","No"],["An area with widespread COVID-19 case (I traveled there)","An area with widespread COVID-19 case (I live there)","I don’t know","None of the above"],["I live with COVID-19 patient","Close Contact (10 mins & above)","Close Contact (6 feet and sneezed on)","Minimal contact with a COVID 19 patient (6 feet away)","Minimal contact with a COVID 19 patient (Not coughed or sneezed one)"]],"answers":["You should call 911. Based on your reported symptoms, you should seek care immediately.",["This tool is intended for people who are at least 18 years old","18-64","65"]]}')
+	var data = JSON.parse('{"quantity":4,"messages":[["Hi!","Welcome to our chat bot. Lets begin by asking some questions."],"","",""],"questions":["Are you experiencing any of these problems?","How old are you?","Are you experiencing any of these problems?","Do you have any of these conditions?","Have you travelled internationally?","In the last 14 days, have you been in an area where COVID-19 is widespread?","Tell us your exposure to covid-19 cases"],"options":[["Severe chest pain","Severe Disorientation","Constant chest pain","Difficulty breathing","Constant lightheadedness","None"],["Under 18","18 - 64","65 or above"],["Fever or sweating","Difficulty breathing","Sore throat","New & worsening cough","Vomiting/ diarrhea","None of the above"],["Asthma","Chronic lung disease","Diabetes","Pregnant?","Kidney failure",["Obesity","Extreme","Moderate"],"Liver Cirrhosis","Weakened immune system",["Heart Failure", "Congestive", "Not Congestive"],"Disease that makes you cough","Disease that makes you harder to cough"],["Yes","No"],["I live in an area where COVID-19 is widespread","I have visited an area where COVID-19 is widespread","I don’t know","None of the above"],["I live with COVID-19 patient","Close Contact (10 mins & above)","Close Contact (6 feet and sneezed on)","Minimal contact with a COVID 19 patient (6 feet away)","Minimal contact with a COVID 19 patient (Not coughed or sneezed one)"]],"answers":["You should call 911. Based on your reported symptoms, you should seek care immediately.",["This tool is intended for people who are at least 18 years old","18-64","65"]]}');
 	function fun(i, callback) {
 		var m = data["messages"][i];
 		var ques = data["questions"][i];
 		var o = data["options"][i];
-		console.log(m);
-		console.log(ques);
-		console.log(o);
 		$("body").append("<div class='conv-grp cg" + String(i+1) + "'>");
 		$(".cg" + String(i+1)).append('<img src="avatar.svg" height="40" width="40">');
 		$(".cg" + String(i+1)).append("<div class='conv'>");
@@ -21,16 +18,38 @@ $( document ).ready(function() {
 			}
 		}
 		$(".cg" + String(i+1) + " .conv").append("<div class='text'>" + ques + "</div><br>");
-			$('html, body').animate({
-		        scrollTop: $(".cg" + String(i+1) + " .conv").offset().top
-		    }, 2000);
+		$('html, body').animate({
+			scrollTop: $(".cg" + String(i+1) + " .conv").offset().top
+		}, 2000);
+
+
+		var sub = "";
 		var s ="";
 		for(var k = 0; k < o.length; k++){
-			s = s + "<div class='option'>" + o[k] + "</div>"
+			if(Array.isArray(o[k])){
+				sub = sub + "<div class='option o_m om" + String(k) + "'>" + o[k][0] + "</div>";
+				for(var z = 1; z < o[k].length; z++){
+					sub += "<div class='option'>" + o[k][z] + "</div>"
+				}
+			}
+			else{s = s + "<div class='option'>" + o[k] + "</div>";}
 		}
-		s  = s + "<div class='proceed'>Proceed</div>"
+		s  = s + "<div class='drawer sub_d'>" + sub + "</div><div class='proceed'>Proceed</div>";
 		var drawer = "<div class='drawer d" + String(i+1) + "'>" + s + "</div>";
 		$("body").append(drawer);
+
+		$(".sub_d").css("display","none");
+		$(".o_m").click(function(){
+			$(".sub_d").css("display","");
+			$(".sub_d .option").click(function(){
+				$(".sub_d").addClass("slideoff").fadeOut(500);
+				setTimeout(function(){
+					$(".sub_d").css("display", "none");
+					$(".sub_d").removeClass("slideoff");
+				}, 500);
+			});
+		});
+
 		$(".option").click(function(){
 			$(this).toggleClass("selected");
 		});
@@ -46,7 +65,8 @@ $( document ).ready(function() {
 			for(var l = 0; l < s_options.length; l++){
 				$(".rmsg" + String(i+1) + " .conv").append("<div class='text'>" + s_options[l] + "</div><br>");
 			}
-			if(i === 0){
+			if(i == 0 && s_options.includes("None") == false){
+
 				$("body").append("<div class='conv-grp cg_sp_1'>");
 				$(".cg_sp_1").append('<img src="avatar.svg" height="40" width="40">');
 				$(".cg_sp_1").append("<div class='conv'>");
@@ -58,7 +78,7 @@ $( document ).ready(function() {
 					$(".cg_sp_2").append('<img src="avatar.svg" height="40" width="40">');
 					$(".cg_sp_2").append("<div class='conv'>");
 					$(".cg_sp_2 .conv").append("<div class='text'>" + "This tool is intended for people who are at least 18 years old." + "</div><br>");
-				}else{ callback(); }
+				}else{callback();}
 			}else{
 				callback();
 			}
@@ -67,42 +87,42 @@ $( document ).ready(function() {
 	}// }
 
 	// hehehe
-
-	fun(0, function(){
-		setTimeout(function(){
-			fun(1, function(){
-				if($(".rmsg2 .conv .text").text() === "65 or above"){
-					setTimeout(function(){
-						fun(3, function(){
-							setTimeout(function(){
-								fun(4, function(){
-									setTimeout(function(){
-										fun(5);
-									}, 500);
-								});
-							}, 500);
-						});
-					}, 500);
-				}else{
-					setTimeout(function(){
-						fun(2, function(){
-							setTimeout(function(){
-								fun(3, function(){
-									setTimeout(function(){
-										fun(4, function(){
-											setTimeout(function(){
-												fun(5);
-											}, 500);
-										});
-									}, 500)
-								});
-							}, 500);
-						});
-					}, 500);
-				}
-			});
-		}, 500);
-	});
-	
+	// fun(0, function(){
+	// 	setTimeout(function(){
+	// 		fun(1, function(){
+	// 			if($(".rmsg2 .conv .text").text() === "65 or above"){
+	// 				setTimeout(function(){
+	// 					fun(3, function(){
+	// 						setTimeout(function(){
+	// 							fun(4, function(){
+	// 								setTimeout(function(){
+	// 									fun(5);
+	// 								}, 500);
+	// 							});
+	// 						}, 500);
+	// 					});
+	// 				}, 500);
+	// 			}
+	// 			else{
+	// 				setTimeout(function(){
+	// 					fun(2, function(){
+	// 						setTimeout(function(){
+	// 							fun(3, function(){
+	// 								setTimeout(function(){
+	// 									fun(4, function(){
+	// 										setTimeout(function(){
+	// 											fun(5);
+	// 										}, 500);
+	// 									});
+	// 								}, 500)
+	// 							});
+	// 						}, 500);
+	// 					});
+	// 				}, 500);
+	// 			}
+	// 		});
+	// 	}, 500);
+	// });
+	fun(3);	
 });
 
---
+++ Bot/js/main2.js
@@ -0,0 +1,84 @@
+var block2 = "<div class=\"block_2\"><div class=\"conv-grp\"><img src=\"avatar.svg\" height=\"40\" width=\"40\"><div class=\"conv\"><div class=\"text\">How old are you?</div></div></div><div class=\"drawer\"><div class=\"option jhamela\">Under 18</div><div class=\"option\">18 - 64</div><div class=\"option\">65 or above</div><div class='proceed'>Proceed</div></div></div>";
+
+$(".option").click(function(){
+	$(this).toggleClass("selected");
+});
+$(".block_2, .block_3, .block_4, .block_5, .block_6, .block_7").hide();
+$(".block_1 .proceed").click(function(){
+	$(".selected").each( function(a){
+		$(".block_1 .right .conv").append("<div class='text'>" + $(".selected").eq(a).text() +"</div><br>");
+		$(".block_1 .drawer").addClass("slideoff").fadeOut(500);
+	});
+	$(".block_2").show();
+});
+$(".block_2 .proceed").click(function(){
+	if($(".block_2 .selected").hasClass("jhamela")){
+		$(".block_2 .conv-grp.left .conv").append("<div class='text'>This tool is intended for people who are at least 18 years old.</div><br>");
+		$(".jhamela").removeClass("selected");
+	}
+	else if($(".block_2 .selected").text() === "18 - 64"){
+		$(".block_2 .drawer").addClass("slideoff").fadeOut(500);
+		$(".block_2 .right .conv").append("<div class='text'>" + $(".block_2 .selected").text() +"</div><br>");
+		$(".block_3").show();
+		$(".block_3 .proceed").click(function(){
+			$(".block_3 .selected").each( function(a){
+				$(".block_3 .right .conv").append("<div class='text'>" + $(".block_3 .selected").eq(a).text() +"</div><br>");
+			});
+			$(".block_3 .drawer").addClass("slideoff").fadeOut(500);
+			$(".block_4").show();
+		});
+	}
+	else{
+		$(".block_2 .drawer").addClass("slideoff").fadeOut(500);
+		$(".block_2 .right .conv").append("<div class='text'>" + $(".block_2 .selected").text() +"</div><br>");
+		$(".block_4").show();
+	}
+});
+$(".block_4 .proceed").click(function(){
+	$(".block_4 .selected").each( function(a){
+		$(".block_4 .right .conv").append("<div class='text'>" + $(".block_4 .selected").eq(a).text() +"</div><br>");
+		$(".block_4 .drawer").addClass("slideoff").fadeOut(500);
+	});
+	$(".block_5").show();
+});
+$(".block_5 .proceed").click(function(){
+	$(".block_5 .selected").each( function(a){
+		$(".block_5 .right .conv").append("<div class='text'>" + $(".block_5 .selected").eq(a).text() +"</div><br>");
+		$(".block_5 .drawer").addClass("slideoff").fadeOut(500);
+	});
+	$(".block_6").show();
+});
+$(".block_6 .proceed").click(function(){
+	$(".block_6 .selected").each( function(a){
+		$(".block_6 .right .conv").append("<div class='text'>" + $(".block_5 .selected").eq(a).text() +"</div><br>");
+		$(".block_6 .drawer").addClass("slideoff").fadeOut(500);
+	});
+	$(".block_7").show();
+});
+// $(".block_4 .proceed").click(function(){
+
+
+
+$(".sub_d").css("display","none");
+
+$(".o_m_1").click(function(){
+	$(".sub_d_1").css("display","");
+	$(".sub_d_1 .option").click(function(){
+		$(".sub_d_1").addClass("slideoff").fadeOut(500);
+		setTimeout(function(){
+			$(".sub_d_1").css("display", "none");
+			$(".sub_d_1").removeClass("slideoff");
+		}, 500);
+	});
+});
+
+$(".o_m_2").click(function(){
+	$(".sub_d_2").css("display","");
+	$(".sub_d_2 .option").click(function(){
+		$(".sub_d_2").addClass("slideoff").fadeOut(500);
+		setTimeout(function(){
+			$(".sub_d_2").css("display", "none");
+			$(".sub_d_2").removeClass("slideoff");
+		}, 500);
+	});
+});
