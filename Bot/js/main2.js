be37d743aef352386ee477da50b2ad720056eb1e
binarysakir <binarysakir@gmail.com>
2020-04-07 03:30:07 +0600

    Initial Commit

--- Bot/js/main2.js
+++ Bot/js/main2.js
@@ -4,7 +4,6 @@ $(".option").click(function(){
 	$(this).toggleClass("selected");
 });
 $(".block_2, .block_3, .block_4, .block_5, .block_6, .block_7, .block_8, .sp_1, .sp_2, .sp_3, .sp_4, .block_sp_1").hide();
-
 $(".block_1 .proceed").click(function(){
 	$(".selected").each( function(a){
 		$(".block_1 .right .conv").append("<div class='text'>" + $(".selected").eq(a).text() +"</div><br>");
@@ -36,12 +35,24 @@ $(".block_2 .proceed").click(function(){
 			});
 			$(".block_3 .drawer").addClass("slideoff").fadeOut(500);
 			$(".block_4").show();
+			$(".block_4 .back").click(function(){
+				$(".block_4").hide();
+				$(".block_3 .drawer").removeClass("slideoff").fadeIn(500);
+			});
+		});
+		$(".block_3 .back").click(function(){
+			$(".block_3").hide();
+			$(".block_2 .drawer").removeClass("slideoff").fadeIn(500);
 		});
 	}
 	else{
 		$(".block_2 .drawer").addClass("slideoff").fadeOut(500);
 		$(".block_2 .right .conv").append("<div class='text'>" + $(".block_2 .selected").text() +"</div><br>");
 		$(".block_4").show();
+		$(".block_4 .back").click(function(){
+			$(".block_4").hide();
+			$(".block_2 .drawer").removeClass("slideoff").fadeIn(500);
+		});
 	}
 });
 $(".block_4 .proceed").click(function(){
@@ -135,6 +146,17 @@ $(".o_m_4").click(function(){
 		setTimeout(function(){
 			$(".sub_d_4").css("display", "none");
 			$(".sub_d_4").removeClass("slideoff");
+		}, 500);
+	});
+});
+
+$(".o_m_5").click(function(){
+	$(".sub_d_5").css("display","");
+	$(".sub_d_5 .option").click(function(){
+		$(".sub_d_4").addClass("slideoff").fadeOut(500);
+		setTimeout(function(){
+			$(".sub_d_5").css("display", "none");
+			$(".sub_d_5").removeClass("slideoff");
 		}, 500);
 	});
 });
