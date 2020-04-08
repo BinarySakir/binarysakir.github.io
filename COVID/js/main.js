4e5a272a9161be2efd7654165b4a683dbaaaea77
binarysakir <binarysakir@gmail.com>
2020-04-08 16:05:14 +0600

    auto scroll

--- COVID/js/main.js
+++ COVID/js/main.js
@@ -1,5 +1,5 @@
 function scrollTo(element){
-	$("html, body").animate({ scrollTop: $(element).offset().top })
+	$("html, body").animate({ scrollTop: $(element).offset().top }, "slow")
 }
 
 $('.sub_option').hide();
@@ -44,7 +44,7 @@ $(".option").click(function(){
 $(".block_1 .proceed").click(function(){
 	$(".block_1 .drawer .proceed").fadeOut(500);
 	if($(".block_1 .drawer .option").hasClass("selected") && $(".block_1 .drawer .option.none").hasClass("selected") === false){
-		setTimeout(function(){$(".sp_block_1").fadeIn(500); scrollTo(".sp_block_1");}, 500)
+		setTimeout(function(){$(".sp_block_1").fadeIn(500); scrollTo(".something");}, 500)
 
 		setTimeout(function(){
 			$(".sp_drawer_1").fadeIn(500);
