90082965ca350fae25d31353ebd851b0636c7a3d
binarysakir <binarysakir@gmail.com>
2020-04-18 00:15:25 +0600

    init

--- Update/js/main.js
+++ Update/js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+	var data = [740, 92, 7, 37, 4, 42, 31, 8]
 	var widthOfCol = $(".container .col")[0].offsetWidth - 10;
 	var drawerHeight = $(window).height() - $(".container").offset().top;
 	var drawer = $(".drawer");
@@ -6,6 +7,12 @@ $(document).ready(function() {
 	$(".district .circle").css("width", widthOfCol+"px");
 	$(".drawer").css("height", drawerHeight+"px");
 	$(".container .col").click(function(){
+
+		// THIS IS FOR DEMO ONLY 
+		var num = $(this).attr("data-num");
+		$(".drawer h1").html($(this).text());
+		$(".drawer h2 span").html(data[num]);
+		
 		drawer.show();
 	});
 	$(document).mouseup(function (e){
